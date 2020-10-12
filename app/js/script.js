b12b8fdd2c0a48520e1d3f249442ab69f2d31919
joebalzac <jgbalzac@gmail.com>
2020-10-12 15:42:00 -0400

    first commit

---
+++ app/js/script.js
@@ -0,0 +1,52 @@
+//fetch local json file
+fetch("productData.json")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    appendData(data);
+  })
+  .catch(function (err) {
+    console.log("error: " + err);
+  });
+//main data function
+function appendData(data) {
+  var mainContainer = document.getElementById("myData");
+
+  //loop
+  for (const [key, value] of Object.entries(data["data"])) {
+    //sub-containers and classes
+    var itemContainer = document.createElement("div");
+    itemContainer.classList.add("item-container");
+    var copyContainer = document.createElement("div");
+    copyContainer.classList.add("copy-container");
+
+    //main container
+    document.getElementById("myData").innerHTML;
+    var title = document.createElement("div");
+    title.classList.add("item-name");
+    var images = document.createElement("img");
+    images.classList.add("images");
+    var price = document.createElement("div");
+    price.classList.add("sale-price");
+    var list = document.createElement("div");
+    list.classList.add("list-price");
+    var button = document.createElement("button");
+
+    //parsing json info
+    title.innerHTML = value.title;
+    images.src = value.images[0].name;
+    list.innerHTML = "$" + value.prices.list;
+    price.innerHTML = "$" + value.prices.price;
+    button.innerHTML = "Add to Cart";
+
+    //appends
+    itemContainer.appendChild(images);
+    copyContainer.appendChild(title);
+    copyContainer.appendChild(list);
+    copyContainer.appendChild(price);
+    itemContainer.appendChild(copyContainer);
+    mainContainer.appendChild(itemContainer);
+    itemContainer.appendChild(button);
+  }
+}
